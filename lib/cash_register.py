b7206deff4fa76a64d992af710c56751618d4f04
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-07-21 10:43:08 -0400

    lab complete

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount = 0):
+    self.discount = discount
+    self.total = 0
+    self.items = []
+
+  def add_item(self, new_item, price, quantity = 1):
+    self.total += price * quantity
+    for i in range(quantity):
+      self.items.append(new_item)
+      self.last_transaction = [new_item, price, quantity]
+
+  def apply_discount(self):
+    self.total -= int(self.total * self.discount / 100)
+    if self.discount == 0:
+      print("There is no discount to apply.")
+    else:
+      print(f"After the discount, the total comes to ${self.total}.")
+
+  def items_list_without_multiples(self):
+    unique_list = list(set(self.items))
+    print(unique_list)
+    return unique_list
+  
+  def items_list_with_multiples(self):
+    return self.items
+  
+  def void_last_transaction(self):
+    transaction = self.last_transaction
+    self.total -= transaction[1] * transaction[2]
+    print(f"{transaction[2]} {transaction[0]}(s) has been removed from the shopping list")
